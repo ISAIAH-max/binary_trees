8d760b7aabe530b42f816027ba89b01acc3fba91
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-18 02:42:14 +0300

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newNode = binary_tree_node(parent, value);
-	if (parent->right == NULL)
+	if (newNode == NULL)
 		return (NULL);
 	if (parent->right != NULL)
 	{
